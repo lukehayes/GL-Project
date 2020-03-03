8d3a96000ba48e7020e080e310c753dcd8948bc1
lukehayes <lukepurplehayes@gmail.com>
2020-03-03 09:33:43 +0000

    Implemented a window wrapper class for SDL_WIndow

--- include/app/window.h
+++ include/app/window.h
@@ -1,11 +1,60 @@
 #ifndef APP_WINDOW_H
 #define APP_WINDOW_H
 
+#include <iostream>
+#include "SDL.h"
+
 namespace App {
 
 	class Window {
-		Window() {}
-		~Window() {}
+
+	public:
+
+		Window(const char* t, int w, int h) 
+			: m_title(t), m_width(w), m_height(h)
+		{
+			this->createWindow();
+
+			// Check that the window was successfully created
+			if (m_window == NULL) {
+				// In the case that the window could not be made...
+				printf("Could not create window: %s\n", SDL_GetError());
+			}
+		}
+
+		~Window() {
+
+			std::cout << "Window Destroyed" << std::endl;
+			SDL_DestroyWindow(m_window);
+		}
+
+		/*
+			Window getter
+		*/
+		void createWindow() {
+			this->m_window = SDL_CreateWindow(
+					m_title,
+					SDL_WINDOWPOS_CENTERED,
+					SDL_WINDOWPOS_CENTERED,
+					m_width,
+					m_height,
+					SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+			);
+		}
+	
+		/*
+			Window getter
+		*/
+		SDL_Window* getWindow() {
+			return this->m_window;
+		}
+
+	private:
+		const char* m_title;
+		int m_width;
+		int m_height;
+
+		SDL_Window* m_window;
 	};
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include "SDL.h"
+#include "app/window.h"
 
 int main(int argc, char* argv[])
 {
@@ -17,31 +18,17 @@ int main(int argc, char* argv[])
 
 	constexpr int WIDTH = 800;
 	constexpr int HEIGHT = 600;
+    
+    App::Window window("Title", 800, 600);
 
-	SDL_Window* window = SDL_CreateWindow(
-            "Game Window",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            800,
-            600,
-            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-    );
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
-
-
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
+	SDL_GLContext glContext = SDL_GL_CreateContext(window.getWindow());
 
 	SDL_Event event;
-    float rt = 0.0;
 
 	while (1) {
 
 		SDL_PollEvent(&event);
+
 		if (event.type == SDL_QUIT) {
 			break;
 		}
@@ -49,11 +36,10 @@ int main(int argc, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(1.0, 1.0, 1.0, 1.0);
 
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(window.getWindow());
 	}
 
     SDL_GL_DeleteContext(glContext);
-	SDL_DestroyWindow(window);
 
     SDL_Quit();
 
