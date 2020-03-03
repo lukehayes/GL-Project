9327e158f91527a1c2ba84528ec9f8663f22f8e3
lukehayes <lukepurplehayes@gmail.com>
2020-03-03 16:34:44 +0000

    Changed __UNIX__ to __APPLE__

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,12 @@
 ﻿#ifdef _WIN32
     #define GLEW_STATIC
     #include "glew.h"
-#elif __UNIX__
-    #include <OpenGL/gl3.h>
+#elif __APPLE__
+    #include "OpenGL/gl3.h"
 #endif
 
-#include <iostream>
-#include <vector>
-#include <string>
 #include "SDL.h"
 #include "app/window.h"
-#include "shaders/shader.h"
-
-void doGL() {
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	std::vector<float> verts {
-	   -1.0f, -1.0f, 0.0f,
-	   1.0f, -1.0f, 0.0f,
-	   0.0f,  1.0f, 0.0f
-	};
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * verts.size(), verts.data(), GL_STATIC_DRAW);
-}
 
 int main(int argc, char* argv[])
 {
@@ -45,25 +19,6 @@ int main(int argc, char* argv[])
 	constexpr int HEIGHT = 600;
     
     App::Window window("Title", 800, 600);
-
-    SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 3); //OpenGL 3+
-    SDL_GL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 3); //OpenGL 3.3
-	SDL_GLContext glContext = SDL_GL_CreateContext(window.getWindow());
-
-    int a;
-    int b;
-
-    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &a);
-    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &b);
-
-    std::cout << a << " " << b << std::endl;
-
-    doGL();
-
-	Shader::Shader shader;
-
-
 
 	SDL_Event event;
 
@@ -78,13 +33,9 @@ int main(int argc, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(1.0, 1.0, 1.0, 1.0);
 
-		glUseProgram(shader.getShaderProgram());
-		glDrawArrays(GL_TRIANGLES,0, 3);
-
         SDL_GL_SwapWindow(window.getWindow());
 	}
 
-    SDL_GL_DeleteContext(glContext);
 
     SDL_Quit();
 
