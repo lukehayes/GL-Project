8bfa257677dc9025cb8be3be653a9d88fd893901
lukehayes <lukepurplehayes@gmail.com>
2020-03-06 09:21:16 +0000

    File contents is now written explicitly if the file is open

--- src/io/file-io.cpp
+++ src/io/file-io.cpp
@@ -15,15 +15,14 @@ namespace IO {
 
 		if (filestream.is_open()) {
 			std::cout << "File: " << file << " opened." << std::endl;
+			// Lazy way of reading in file contents
+			// but it works
+			contents << filestream.rdbuf();
 		}
 		else {
 			std::cout << "File: " << file << " could not be opened."<< std::endl;
 			return "Reading Failed";
 		}
-
-		// Lazy way of reading in file contents
-		// but it works
-		contents << filestream.rdbuf();
 
 		return contents.str();
 	}
